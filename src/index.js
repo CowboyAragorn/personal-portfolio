231db562179bc781129dc1cff024d2146b053d21
CowboyAragorn <jacobman0313@gmail.com>
2022-11-29 16:23:49 -0500

    final touches

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import "./fonts/modernNo20.ttf";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
